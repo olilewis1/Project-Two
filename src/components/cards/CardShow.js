2f1d8953201b27a33c590a6a0cc2e05871658633
Anna Monkman <annamonkman@me.com>
2021-03-11 19:41:27 +0000

    game logic

--- src/components/cards/CardShow.js
+++ src/components/cards/CardShow.js
@@ -10,18 +10,14 @@ const CardShow = ( ) => {
   const [cardFaceUp, setCardFaceUp] = useState('')
   const [cardFaceDown, setCardFaceDown] = useState('')
 
-  console.log('cards', cards)
-  console.log(cardFaceUp, setCardFaceUp)
-  console.log('cardfacedown', cardFaceDown)
-  console.log('setcardfacedown', setCardFaceDown)
+  const [eventShow, setEventShow] = useState('')
 
-  
+  console.log(eventShow, setEventShow)
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get('https://swapi.dev/api/starships/')
       console.log('DATA', response.data.results)
       setCards(response.data.results)
-      console.log('set cards', setCards)
     }
     getData()
   }, [])
@@ -31,6 +27,7 @@ const CardShow = ( ) => {
     if (!cards) return null
     setCardFaceUp(cards[Math.floor(Math.random() * 10)])
     console.log('card face up random', cardFaceUp)
+
   }, [cards])
 
   useEffect(() => {
@@ -41,9 +38,44 @@ const CardShow = ( ) => {
 
   const handleSubmit = event => {
     event.preventDefault()
-    console.log('EVENT', event)
+    console.log('EVENT', (event.target.value))
+    const eventNumber = event.target.value
+    if (eventNumber === undefined) {
+      return null && console.log('you lost')
+    }
+
+    // * WIN / LOSE / DRAW LOGIC
+
+    // * PASSENGERS
+    
+    if (eventNumber.split('').indexOf(',') !== -1 || eventNumber.split('').indexOf('/') !== -1 ){
+      console.log('contains a comma or a n/a')
+      if (eventNumber.split('').indexOf(',') !== -1) { 
+        return setEventShow(eventNumber.replace(/,/g, '')), console.log('newevent, no comma', typeof eventShow)
+      } else { 
+        console.log('im n/a')
+      }
+    } else if (eventNumber.split('').indexOf(',') === -1) {
+      console.log('all good doesnt contain comma')
+    }
+    if (eventNumber === 'n/a') {
+      console.log('replace n/a', eventNumber.replace(/(n\/a)+/g, '0'))
+    }
+  
+    if (parseInt(eventNumber) > parseInt(cardFaceDown.passengers)) {
+      console.log('you won')
+    }
+    if (parseInt(eventNumber) < parseInt(cardFaceDown.passengers)) {
+      console.log('you lost')
+    }
+    if (parseInt(eventNumber) === parseInt(cardFaceDown.passengers)) {
+      console.log('draw')
+    }
+    console.log('type of', typeof eventNumber)
   }
 
+
+  
   if (!cards) return null 
 
   return (
@@ -53,7 +85,7 @@ const CardShow = ( ) => {
         <div className="card-container">
           <div onClick={handleSubmit} className="card-single-container">
             <button className="card-button"> Name - <span className="Data">{cardFaceUp.name} </span></button>
-            <button className="card-button"> Passengers - <span className="Data">{cardFaceUp.passengers} </span></button>
+            <button className="card-button" value={cardFaceUp.passengers} > Passengers - <span className="Data">{cardFaceUp.passengers} </span></button>
             <button className="card-button"> Speed - <span className="Data">{cardFaceUp.max_atmosphering_speed} </span></button>
             <button className="card-button"> Cost - <span className="Data">{cardFaceUp.cost_in_credits} </span></button>
             <button className="card-button"> Length - <span className="Data">{cardFaceUp.length} </span></button>
