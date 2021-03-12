6894ffc0cc07b83ca46ac98758c74429d0b5d766
olilewis1 <olilewis1@hotmail.co.uk>
2021-03-12 09:36:49 +0000

    TT- the event.target.name is working,

--- src/components/cards/CardShow.js
+++ src/components/cards/CardShow.js
@@ -11,16 +11,15 @@ const CardShow = ( ) => {
   const [cardFaceDown, setCardFaceDown] = useState('')
 
   const [eventNumber, setEventNumber] = useState('')
-  const [result, setResult] = useState('')
-  console.log(result, setResult)
+  // const [result, setResult] = useState('')
+  // console.log(result, setResult)
 
-  const win = 'win'
+  // const win = 'win'
   // * GET DATA
 
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get('https://swapi.dev/api/starships/')
-      console.log('DATA', response.data.results)
       setCards(response.data.results)
     }
     getData()
@@ -30,14 +29,12 @@ const CardShow = ( ) => {
   useEffect(() => { 
     if (!cards) return null
     setCardFaceUp(cards[Math.floor(Math.random() * 10)])
-    console.log('card face up random', cardFaceUp)
 
   }, [cards])
 
   useEffect(() => {
     if (!cards) return null
     setCardFaceDown(cards[Math.floor(Math.random() * 10)])
-    console.log('card face up random', cardFaceDown)
   }, [cards])
 
   
@@ -57,72 +54,79 @@ const CardShow = ( ) => {
   const handleSubmit = event => {
     event.preventDefault()
     console.log('EVENT', (event.target.value))
-    console.log('event.name', event.target.name)
-    setResult(event.target.name)
     console.log('card face down', cardFaceDown.[event.target.name])
-    setEventNumber(event.target.value)
-    if (eventNumber === undefined) {
-      return null && console.log('you lost')
+    setEventNumber(parseInt(event.target.value)), console.log( typeof eventNumber)
+    //Sort out numbers
+
+    if (eventNumber === undefined) return null
+    
+    // if (eventNumber.split('').indexOf(',') !== -1) { 
+    //   return setEventNumber(eventNumber.replace(/,/g, '')), console.log(eventNumber) 
+    // } setEventNumber(Number(event.target.value))
+    if (event.target.value > cardFaceDown.[event.target.name]) { 
+      console.log('you win')
     } 
+    
+    
 
     // * WIN / LOSE / DRAW LOGIC
 
     // * PASSENGERS
     
-    if (eventNumber.split('').indexOf(',') !== -1 || eventNumber.split('').indexOf('/') !== -1 ){
-      console.log('contains a comma or a n/a')
-      if (eventNumber.split('').indexOf(',') !== -1) { 
-        return setEventNumber(eventNumber.replace(/,/g, '')), console.log('newevent, no comma', typeof eventShow)
-      } else { 
-        console.log('im n/a')
-      }
-    } else if (eventNumber.split('').indexOf(',') === -1) {
-      console.log('all good doesnt contain comma')
-    }
+    // if (eventNumber.split('').indexOf(',') !== -1 || eventNumber.split('').indexOf('/') !== -1 ){
+    //   console.log('contains a comma or a n/a')
+    //   if (eventNumber.split('').indexOf(',') !== -1) { 
+    //     return setEventNumber(eventNumber.replace(/,/g, '')), console.log('newevent, no comma', typeof eventShow)
+    //   } else { 
+    //     console.log('im n/a')
+    //   }
+    // } else if (eventNumber.split('').indexOf(',') === -1) {
+    //   console.log('all good doesnt contain comma')
+    // }
 
-    if (eventNumber === 'n/a') {
-      console.log('replace n/a', eventNumber.replace(/(n\/a)+/g, '0'))
-    }
-    if (cardFaceDown.passengers === 'n/a') {
-      console.log('replace n/a card down ', cardFaceDown.passengers.replace(/(n\/a)+/g, '0'))
-    }
+    // if (eventNumber === 'n/a') {
+    //   console.log('replace n/a', eventNumber.replace(/(n\/a)+/g, '0'))
+    // }
+    // if (cardFaceDown.passengers === 'n/a') {
+    //   console.log('replace n/a card down ', cardFaceDown.passengers.replace(/(n\/a)+/g, '0'))
+    // }
   
-    if (parseInt(eventNumber) > parseInt(cardFaceDown.passengers)) {
-      console.log('you won')
-      setResult(win)
-    }
-    if (parseInt(eventNumber) < parseInt(cardFaceDown.passengers)) {
-      console.log('you lost')
-    }
-    if (parseInt(eventNumber) === parseInt(cardFaceDown.passengers)) {
-      console.log('draw')
-    }
-    console.log('type of', typeof eventNumber)
+    // if (parseInt(eventNumber) > parseInt(cardFaceDown.passengers)) {
+    //   console.log('you won')
+    //   setResult(win)
+    // }
+    // if (parseInt(eventNumber) < parseInt(cardFaceDown.passengers)) {
+    //   console.log('you lost')
+    // }
+    // if (parseInt(eventNumber) === parseInt(cardFaceDown.passengers)) {
+    //   console.log('draw')
+    // }
+    // console.log('type of', typeof eventNumber)
   }
 
   // * MGLT
 
-  if (parseInt(eventNumber) > parseInt(cardFaceDown.MGLT)) {
-    console.log( 'type of', typeof eventNumber)
-    console.log('you won')
-  }
-  if (parseInt(eventNumber) < parseInt(cardFaceDown.MGLT)) {
-    console.log('you lost')
-    console.log( 'type of', typeof eventNumber)
-  }
-  if (parseInt(eventNumber) === parseInt(cardFaceDown.MGLT)) {
-    console.log('draw')
-    console.log( 'type of', typeof eventNumber)
-  }
+  // if (parseInt(eventNumber) > parseInt(cardFaceDown.MGLT)) {
+  //   console.log( 'type of', typeof eventNumber)
+  //   console.log('you won')
+  // }
+  // if (parseInt(eventNumber) < parseInt(cardFaceDown.MGLT)) {
+  //   console.log('you lost')
+  //   console.log( 'type of', typeof eventNumber)
+  // }
+  // if (parseInt(eventNumber) === parseInt(cardFaceDown.MGLT)) {
+  //   console.log('draw')
+  //   console.log( 'type of', typeof eventNumber)
+  // }
 
   // * COST IN CREDITS
 
-  if (eventNumber > cardFaceDown.cost_in_credits || cardFaceDown.cost_in_credits === 'unknown') { 
-    console.log('you win cost in credits')
-  } 
-  if (eventNumber < cardFaceDown.cost_in_credits || eventNumber === 'unknown') {
-    console.log('you won')
-  } 
+  // if (eventNumber > cardFaceDown.cost_in_credits || cardFaceDown.cost_in_credits === 'unknown') { 
+  //   console.log('you win cost in credits')
+  // } 
+  // if (eventNumber < cardFaceDown.cost_in_credits || eventNumber === 'unknown') {
+  //   console.log('you won')
+  // } 
 
   // * LENGTH
 
@@ -133,29 +137,29 @@ const CardShow = ( ) => {
   //   console.log('doesnt contain a comma')
   // }
 
-  if (parseInt(eventNumber) > parseInt(cardFaceDown.length)) {
-    console.log( 'type of', typeof eventNumber)
-    console.log('you won')
-  }
-  if (parseInt(eventNumber) < parseInt(cardFaceDown.length)) {
-    console.log('you lost')
-    console.log( 'type of', typeof eventNumber)
-  }
+  // if (parseInt(eventNumber) > parseInt(cardFaceDown.length)) {
+  //   console.log( 'type of', typeof eventNumber)
+  //   console.log('you won')
+  // }
+  // if (parseInt(eventNumber) < parseInt(cardFaceDown.length)) {
+  //   console.log('you lost')
+  //   console.log( 'type of', typeof eventNumber)
+  // }
 
   // * HYPERDRIVE RATING
 
-  if (parseInt(eventNumber) > parseInt(cardFaceDown.hyperdrive_rating)) {
-    console.log( 'type of hyperdrive', typeof eventNumber)
-    console.log('you won hyperdrive')
-  }
-  if (parseInt(eventNumber) < parseInt(cardFaceDown.hyperdrive_rating)) {
-    console.log('you lost hyperdrive')
-    console.log( 'type of hyperdrive', typeof eventNumber)
-  }
-  if (parseInt(eventNumber) === parseInt(cardFaceDown.hyperdrive_rating)) {
-    console.log('draw hyperdrive')
-    console.log( 'type of hyperdrive', typeof eventNumber)
-  }
+  // if (parseInt(eventNumber) > parseInt(cardFaceDown.hyperdrive_rating)) {
+  //   console.log( 'type of hyperdrive', typeof eventNumber)
+  //   console.log('you won hyperdrive')
+  // }
+  // if (parseInt(eventNumber) < parseInt(cardFaceDown.hyperdrive_rating)) {
+  //   console.log('you lost hyperdrive')
+  //   console.log( 'type of hyperdrive', typeof eventNumber)
+  // }
+  // if (parseInt(eventNumber) === parseInt(cardFaceDown.hyperdrive_rating)) {
+  //   console.log('draw hyperdrive')
+  //   console.log( 'type of hyperdrive', typeof eventNumber)
+  // }
 
   
   if (!cards) return null 
@@ -173,7 +177,7 @@ const CardShow = ( ) => {
             <button className="card-button" name="MGLT" onClick={handleSubmit}  value={cardFaceUp.MGLT} > Megalights per hour - <span className="Data">{cardFaceUp.MGLT} </span></button>
             <button className="card-button" name="cost_in_credits" onClick={handleSubmit}  value={cardFaceUp.cost_in_credits} > Cost - <span className="Data">{cardFaceUp.cost_in_credits} </span></button>
             <button className="card-button" name="length" onClick={handleSubmit} value={cardFaceUp.length}> Length - <span className="Data">{cardFaceUp.length} </span></button>
-            <button className="card-button" name="hyperdrive_rating" Click={handleSubmit} value={cardFaceUp.hyperdrive_rating}> Hyperdrive Rating - <span className="Data">{cardFaceUp.hyperdrive_rating} </span></button>
+            <button className="card-button" name="hyperdrive_rating" onClick={handleSubmit} value={cardFaceUp.hyperdrive_rating}> Hyperdrive Rating - <span className="Data">{cardFaceUp.hyperdrive_rating} </span></button>
           </div>
         </div>
 
